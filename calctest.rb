d80bc932ff1c996771a769d28394af6e6d315475
JBrow95 <browningjustin95@gmail.com>
2018-06-19 15:17:36 -0400

    made test func for subtract and multiply

--- calctest.rb
+++ calctest.rb
@@ -1,20 +1,63 @@
-def int_test(int1, int2)
-	if int1 == int2
-		puts true
-	else
-		puts false
-	end
-end
-int_test(1,2)
-int_test(1,1)
 
-def int_item(int1, int2)
-	if int1.class == int2.class
+
+#testing begins
+
+def value_can_add_another_value()
+	v1 = 1
+	v2 = 1
+	vtotal = v1 + v2
+
+	if vtotal == 2
 		puts true
 	else
 		puts false
 	end
 end
-int_item(1,2)
-int_item(1,"1")
-int_item(1,[])
+value_can_add_another_value()
+
+def value_can_subtract_another_value()
+	v1 = 1
+	v2 = 1
+	vtotal = v1 - v2
+
+	if vtotal == 0
+		puts true
+	else
+		puts false
+	end
+end
+value_can_subtract_another_value()
+
+def value_can_multiply_another_value()
+	v1 = 1
+	v2 = 2
+	vtotal = v1 * v2
+
+	if vtotal == 2
+		puts true
+	else
+		puts false
+	end
+end
+value_can_multiply_another_value()
+
+# def int_test(int1, int2)
+# 	if int1 == int2
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# int_test(1,2)
+# int_test(1,1)
+
+# def int_item(int1, int2)
+# 	if int1.class == int2.class
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# int_item(1,2)
+# int_item(1,"1")
+# int_item(1,[])
