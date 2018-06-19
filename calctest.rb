9375bf283189198c8a48738e7816bdf38b89ee70
JBrow95 <browningjustin95@gmail.com>
2018-06-19 14:50:53 -0400

    made item test func for int

--- calctest.rb
+++ calctest.rb
@@ -1,8 +1,20 @@
-def int_item_test(int1, int2)
-	if int1.class == int2.class
+def int_test(int1, int2)
+	if int1 == int2
 		puts true
 	else
 		puts false
 	end
 end
 int_test(1,2)
+int_test(1,1)
+
+def int_item(int1, int2)
+	if int1.class == int2.class
+		puts true
+	else
+		puts false
+	end
+end
+int_item(1,2)
+int_item(1,"1")
+int_item(1,[])
