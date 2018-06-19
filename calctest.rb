08623135efa4e4390e0a204e6f8e74cb178d79b7
JBrow95 <browningjustin95@gmail.com>
2018-06-19 15:40:33 -0400

    made divide by zero error test func

--- calctest.rb
+++ calctest.rb
@@ -1,76 +1,87 @@
 
 
 #testing begins
+def divide_by_0error()
+	v1 = gets.chomp.to_i
+	v2 = gets.chomp.to_i
 
-def value_can_add_another_value()
-	v1 = 1
-	v2 = 1
-	vtotal = v1 + v2
-
-	if vtotal == 2
-		puts true
+	if v2 == 0
+		puts "cant divide by zero"
 	else
 		puts false
 	end
 end
-value_can_add_another_value()
+divide_by_0error()
 
-def value_can_subtract_another_value()
-	v1 = 1
-	v2 = 1
-	vtotal = v1 - v2
+# def value_can_add_another_value()
+# 	v1 = 1
+# 	v2 = 1
+# 	vtotal = v1 + v2
 
-	if vtotal == 0
-		puts true
-	else
-		puts false
-	end
-end
-value_can_subtract_another_value()
+# 	if vtotal == 2
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# value_can_add_another_value()
 
-def value_can_multiply_another_value()
-	v1 = 1
-	v2 = 2
-	vtotal = v1 * v2
+# def value_can_subtract_another_value()
+# 	v1 = 1
+# 	v2 = 1
+# 	vtotal = v1 - v2
 
-	if vtotal == 2
-		puts true
-	else
-		puts false
-	end
-end
-value_can_multiply_another_value()
+# 	if vtotal == 0
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# value_can_subtract_another_value()
 
-def value_can_divide_another_value()
-	v1 = 2
-	v2 = 1
-	vtotal = 2
+# def value_can_multiply_another_value()
+# 	v1 = 1
+# 	v2 = 2
+# 	vtotal = v1 * v2
 
-	if vtotal == 2
-		puts true
-	else
-		puts false
-	end
-end
-value_can_divide_another_value()
+# 	if vtotal == 2
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# value_can_multiply_another_value()
 
-def int_test(int1, int2)
-	if int1 == int2
-		puts true
-	else
-		puts false
-	end
-end
-int_test(1,2)
-int_test(1,1)
+# def value_can_divide_another_value()
+# 	v1 = 2
+# 	v2 = 1
+# 	vtotal = 2
 
-def int_item(int1, int2)
-	if int1.class == int2.class
-		puts true
-	else
-		puts false
-	end
-end
-int_item(1,2)
-int_item(1,"1")
-int_item(1,[])
+# 	if vtotal == 2
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# value_can_divide_another_value()
+
+# def int_test(int1, int2)
+# 	if int1 == int2
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# int_test(1,2)
+# int_test(1,1)
+
+# def int_item(int1, int2)
+# 	if int1.class == int2.class
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
+# int_item(1,2)
+# int_item(1,"1")
+# int_item(1,[])
