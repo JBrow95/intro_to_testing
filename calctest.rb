276806d617632ca06b6ad95e987d9eccc22ceb6e
JBrow95 <browningjustin95@gmail.com>
2018-06-19 15:20:29 -0400

    made test func for divide

--- calctest.rb
+++ calctest.rb
@@ -41,23 +41,36 @@ def value_can_multiply_another_value()
 end
 value_can_multiply_another_value()
 
-# def int_test(int1, int2)
-# 	if int1 == int2
-# 		puts true
-# 	else
-# 		puts false
-# 	end
-# end
-# int_test(1,2)
-# int_test(1,1)
+def value_can_divide_another_value()
+	v1 = 2
+	v2 = 1
+	vtotal = 2
 
-# def int_item(int1, int2)
-# 	if int1.class == int2.class
-# 		puts true
-# 	else
-# 		puts false
-# 	end
-# end
-# int_item(1,2)
-# int_item(1,"1")
-# int_item(1,[])
+	if vtotal == 2
+		puts true
+	else
+		puts false
+	end
+end
+value_can_divide_another_value()
+
+def int_test(int1, int2)
+	if int1 == int2
+		puts true
+	else
+		puts false
+	end
+end
+int_test(1,2)
+int_test(1,1)
+
+def int_item(int1, int2)
+	if int1.class == int2.class
+		puts true
+	else
+		puts false
+	end
+end
+int_item(1,2)
+int_item(1,"1")
+int_item(1,[])
